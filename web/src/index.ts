e2b4b2665f273132e2c8b915ad2ed7373f84f16f
김영후 <107921227+Ndream-KimYoungHoo@users.noreply.github.com>
2022-09-28 20:26:11 +0900

    initial

---
+++ web/src/index.ts
@@ -0,0 +1,12 @@
+const world = "world";
+
+export function hello(who: any): string {
+  const a = false;
+
+  if(a) {
+    const b = 2;
+  } else if(a) {
+      const c = 3;
+    }
+  return `Hello ${who}! `
+}
